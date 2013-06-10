d07a2222363d8aabee8503e3a79675105844663c
Jean <jadekler@gmail.com>
2013-06-10 10:22:59 -0400

    Template commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<script type="text/javascript" src="js/jquery/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-ui/ui/jquery-ui.js"></script>
+	<script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<h1>This is a demo <small>of checking pages for changes</small></h1>
+			<span>
+				Explanation
+			</span>
+		</div>
+	</div>
+</body>
+</html>
