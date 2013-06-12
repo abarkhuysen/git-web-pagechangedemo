99dcdb88342595ad93bec14c0ec483283bf3d28d
Jean <jadekler@gmail.com>
2013-06-12 09:54:02 -0400

    Changed the YQL query to use data content instead of html. Catches more pages by reducing the amount of 403 errors

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ $.widget('pagechangedemo.linkComparisonTool', {
 	 * URLs and their page contents
 	 * @return {void}
 	 */
-	_init: function() {
+	 _init: function() {
 		// Basic init
 		var state = this;
 		state.options.thisTable = $(this.element[0]).find('.urlTable');
@@ -41,8 +41,8 @@ $.widget('pagechangedemo.linkComparisonTool', {
 	 * @param  {DOM Element} row
 	 * @return {void}
 	 */
-	analyzePage: function(row)
-	{
+	 analyzePage: function(row)
+	 {
 		// Get the details of the row we're analyzing
 		var state = this;
 		var rowUrl = "http://"+$(row).attr('data-url');
@@ -50,16 +50,19 @@ $.widget('pagechangedemo.linkComparisonTool', {
 		var oldMD5 = $(row).attr('data-md5hash');
 
 		// Attempt to retrieve the page data
-		$.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+
-			encodeURIComponent(rowUrl)+"%22&format=json'&callback=?", function(data) {
-				if(data.results[0]) {
+		if(rowUrl.length > 7) {
+			$.getJSON("http://query.yahooapis.com/v1/public/yql?q="+
+				"select * from html where url='"+
+				encodeURIComponent(rowUrl)+"'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", function(data) {
+					var returnedPage = JSON.stringify(data.query.results);
+					if(returnedPage) {
 					// PAGE EXISTS
 					
 					// Render the checkbox to show it exists
 					state.renderUrlExists(row, true);
 
 					// MD5 the page data for later comparison
-					currentMD5 = md5(data.results[0]);
+					currentMD5 = md5(returnedPage);
 
 					// Store the MD5 of the page data
 					// NOTE: This to be added to a button
@@ -93,30 +96,59 @@ $.widget('pagechangedemo.linkComparisonTool', {
 					}
 				} else {
 					// PAGE DOES NOT EXIST
-
-					state.options.pagesDNE++;
-					$(state.options.fillInPagesDNE).text(state.options.pagesDNE);
-
-					// Render the checkbox to show it doesn't exist
-					state.renderUrlExists(row, false);
-					state.renderMD5Comparison(row, "unknown");
+					state.actionPageDNE(state, row);
 				}
 
-				state.options.completedComparisons++;
-				$(state.options.fillInPagesCompared).text(state.options.completedComparisons);
-				state.renderProgressBar(state);
+				state.actionCompleteAnalysis(state);
 			}
 			);
+} else {
+	state.actionPageDNE(state, row);
+	state.actionCompleteAnalysis(state);
+}
+},
+
+	/**
+	 * This function is the set of actions performed each time a page is Not Found - the count of pages not found increases
+	 * and output, the url exists? box is modified to display red, and the comparison box is grayed out with a question mark
+	 * @param  {scope} state The scope holder
+	 * @param  {[type]} row  The row to perform the action on
+	 * @return void
+	 */
+	 actionPageDNE: function(state, row) {
+	 	state.options.pagesDNE++;
+	 	$(state.options.fillInPagesDNE).text(state.options.pagesDNE);
+
+		// Render the checkbox to show it doesn't exist
+		state.renderUrlExists(row, false);
+		state.renderMD5Comparison(row, "unknown");
 	},
 
-	renderProgressBar: function(state) {
-		var numRows = $(state.options.tableRows).length;
-		var currentPercent = state.options.completedComparisons / numRows * 100;
-		$(state.options.progressBar).css('width', currentPercent+"%");
+	/**
+	 * This function is the set of actions performed at the end of each analysis - increase completeComparisons, output 
+	 * the amount of completed comparisons, and render the progress bar
+	 * @param  {scope} state The scope holder
+	 * @return void
+	 */
+	 actionCompleteAnalysis: function(state) {
+	 	state.options.completedComparisons++;
+	 	$(state.options.fillInPagesCompared).text(state.options.completedComparisons);
+	 	state.renderProgressBar(state);
+	 },
 
-		if(numRows == state.options.completedComparisons)
-			$(state.options.progressBarFull).removeClass('progress-striped');
-	},
+	/**
+	 * This function moves the progress bar by checking how many pages have been compared versus the total amount of pages
+	 * @param  {scope} state The scope holder
+	 * @return void
+	 */
+	 renderProgressBar: function(state) {
+	 	var numRows = $(state.options.tableRows).length;
+	 	var currentPercent = state.options.completedComparisons / numRows * 100;
+	 	$(state.options.progressBar).css('width', currentPercent+"%");
+
+	 	if(numRows == state.options.completedComparisons)
+	 		$(state.options.progressBarFull).removeClass('progress-striped');
+	 },
 
 	/**
 	 * This function renders the Exists? badges to show whether a page exists or not
@@ -124,15 +156,15 @@ $.widget('pagechangedemo.linkComparisonTool', {
 	 * @param  {Boolean} 	 exists
 	 * @return {void}
 	 */
-	renderUrlExists: function(row, exists) {
-		if(exists) {
-			$(row).children("td.url-status").children('span').attr('class', 'badge badge-success')
-			.children('i').attr('class', 'icon-ok');
-		} else {
-			$(row).children("td.url-status").children('span').attr('class', 'badge badge-important')
-			.children('i').attr('class', 'icon-remove');
-		}
-	},
+	 renderUrlExists: function(row, exists) {
+	 	if(exists) {
+	 		$(row).children("td.url-status").children('span').attr('class', 'badge badge-success')
+	 		.children('i').attr('class', 'icon-ok');
+	 	} else {
+	 		$(row).children("td.url-status").children('span').attr('class', 'badge badge-important')
+	 		.children('i').attr('class', 'icon-remove');
+	 	}
+	 },
 
 	/**
 	 * This function renders the Changed? badges to show whether a page has been changed or not
@@ -140,20 +172,20 @@ $.widget('pagechangedemo.linkComparisonTool', {
 	 * @param  {string} 	  isSame
 	 * @return {void}
 	 */
-	renderMD5Comparison: function(row, isSame) {
-		switch(isSame) {
-			case "same":
-				$(row).children("td.url-md5compare").children('span')
-				.children('i').attr('class', 'icon-ok');
-				break;
-			case "not same":
-				$(row).children("td.url-md5compare").children('span').attr('class', 'badge badge-warning')
-				.children('i').attr('class', 'icon-pencil');
-				break;
-			case "unknown":
-				$(row).children("td.url-md5compare").children('span')
-				.children('i').attr('class', 'icon-question');
-				break;
-		}
-	}
-});
+	 renderMD5Comparison: function(row, isSame) {
+	 	switch(isSame) {
+	 		case "same":
+	 		$(row).children("td.url-md5compare").children('span')
+	 		.children('i').attr('class', 'icon-ok');
+	 		break;
+	 		case "not same":
+	 		$(row).children("td.url-md5compare").children('span').attr('class', 'badge badge-warning')
+	 		.children('i').attr('class', 'icon-pencil');
+	 		break;
+	 		case "unknown":
+	 		$(row).children("td.url-md5compare").children('span')
+	 		.children('i').attr('class', 'icon-question-sign');
+	 		break;
+	 	}
+	 }
+	});
